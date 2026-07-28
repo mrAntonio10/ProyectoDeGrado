6b71b1821f005724a13a400c63bc8401703fcefe
mroca <mroca@servicios.mc4.com.bo>
2026-07-28 11:05:43 -0400

    feature: productos- prendas o consumibles[

--- model/src/main/java/com/upb/models/product/Product.java
+++ model/src/main/java/com/upb/models/product/Product.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
@@ -30,13 +31,46 @@ public class Product implements Serializable {
     @Column(name = "CATEGORY", length = 30, nullable = false)
     private String category;
 
+    /*Segmentos de consumibles*/
     @Column(name = "BEVERAGE_FORMAT", length = 30)
     private String beverageFormat;
 
+    /* Segmentos de prendas */
+    @Column(name = "CLOTHING_SIZE", length = 15)
+    private String size;
+
+    @Column(name = "COLOR", length = 30)
+    private String color;
+
+    @Column(name = "GENDER", length = 30)
+    private String gender;
+
+    @Column(name = "BRAND", length = 60)
+    private String brand;
+
+    @Column(name = "PURCHASE_PRICE")
+    private BigDecimal purchasePrice;
+
+    @Column(name = "SALE_PRICE")
+    private BigDecimal salePrice;
+
+    @Column(name = "RECEPTION_DATE")
+    private Long receptionDate;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="ID_SUPPLIER", referencedColumnName = "ID")
+    private com.upb.models.supplier.Supplier supplier;
+
+    @Column(name = "COMMISSION_PERCENTAGE")
+    private BigDecimal commissionPercentage;
+
+    /* Datos Generales */
     @Column(name = "STATE")
     private Boolean state;
 
-    @Column(name = "SKU", length = 6)
+    //ALTER TABLE product ALTER COLUMN sku TYPE varchar(50);
+    @Column(name = "SKU", length = 50)
     private String sku;
 
     @JsonIgnore
